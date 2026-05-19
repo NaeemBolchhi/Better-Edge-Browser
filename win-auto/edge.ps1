c7dfdced50ccaf636015819b8ac14650399cf66e
NaeemBolchhi <naeem.bolchhi@gmail.com>
2026-05-19 21:13:51 +0600

    one-liner trial 2

--- win-auto/edge.ps1
+++ win-auto/edge.ps1
@@ -1,17 +1,16 @@
 # Enable TLSv1.2 for compatibility with older clients
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 
-$ElevateURL = 'https://naeembolchhi.github.io/Better-Edge-Browser/win-auto/elevate.cmd'
-$AddREG = 'https://naeembolchhi.github.io/Better-Edge-Browser/edge-add.reg'
-$RemoveREG = 'https://naeembolchhi.github.io/Better-Edge-Browser/edge-remove.reg'
+$DateVar    = Get-Date -Format 'yyyyMMddHHmmss'
+$ElevateCMD = 'https://naeembolchhi.github.io/Better-Edge-Browser/win-auto/elevate.cmd?v=' + $DateVar
+$AddREG     = 'https://naeembolchhi.github.io/Better-Edge-Browser/edge-add.reg?v=' + $DateVar
+$RemoveREG  = 'https://naeembolchhi.github.io/Better-Edge-Browser/edge-remove.reg?v=' + $DateVar
 
-$Path_AddREG = "$env:TEMP\TmFlZW1Cb2xjaGhp\edge-add.reg"
-$Path_RemoveREG = "$env:TEMP\TmFlZW1Cb2xjaGhp\edge-remove.reg"
-$Path_RunAddREG = "$env:TEMP\TmFlZW1Cb2xjaGhp\edge-add.cmd"
-$Path_RunRemoveREG = "$env:TEMP\TmFlZW1Cb2xjaGhp\edge-remove.cmd"
-$Path_RunSpecialREG = "$env:TEMP\TmFlZW1Cb2xjaGhp\edge-special.cmd"
-
-$customAddon = $args[0]
+$TargetDir       = "$env:TEMP\TmFlZW1Cb2xjaGhp"
+$Path_ElevateCMD = "$TargetDir\edge-elevate.cmd"
+$Path_AddREG     = "$TargetDir\edge-add.reg"
+$Path_RemoveREG  = "$TargetDir\edge-remove.reg"
+$Path_ReplaceREG = "$TargetDir\edge-replace.reg"
 
 if (Test-Path "$env:TEMP\TmFlZW1Cb2xjaGhp\") {
     Remove-Item -Path "$env:TEMP\TmFlZW1Cb2xjaGhp" -Recurse -Force
@@ -20,18 +19,54 @@ if (Test-Path "$env:TEMP\TmFlZW1Cb2xjaGhp\") {
 New-Item -Path "$env:TEMP\" -Name "TmFlZW1Cb2xjaGhp" -ItemType Directory
 
 try {
-	Invoke-WebRequest -Uri $AddREG -UseBasicParsing -OutFile $Path_AddREG
-	Invoke-WebRequest -Uri $RemoveREG -UseBasicParsing -OutFile $Path_RemoveREG
-    Invoke-WebRequest -Uri $ElevateURL -UseBasicParsing -OutFile $Path_RunAddREG
-	Copy-Item -Path $Path_RunAddREG -Destination $Path_RunRemoveREG
-	Copy-Item -Path $Path_RunAddREG -Destination $Path_RunSpecialREG
+    # CMD Files
+    Invoke-WebRequest -Uri $ElevateCMD -UseBasicParsing -OutFile $Path_ElevateCMD -ErrorAction Stop
+    # REG Files
+	Invoke-WebRequest -Uri $AddREG -UseBasicParsing -OutFile $Path_AddREG -ErrorAction Stop
+	Invoke-WebRequest -Uri $RemoveREG -UseBasicParsing -OutFile $Path_RemoveREG -ErrorAction Stop
 } catch {
     Write-Error $_
 	Return
 }
 
-if (Test-Path $FilePath) {
-    Start-Process $FilePath -Wait
-    $item = Get-Item -LiteralPath $FilePath
-    $item.Delete()
+$FilePaths = @(
+    $Path_ElevateCMD,
+    $Path_AddREG,
+    $Path_RemoveREG
+)
+
+if ((Test-Path $FilePaths) -notcontains $false) {
+    if ([string]::IsNullOrEmpty($args[0])) {
+        # No arguments; add edge policies
+        Add-Content -Path $Path_ElevateCMD -Value "reg import `"$Path_AddREG`""
+        Start-Process $Path_ElevateCMD -Wait
+    
+    } elseif ($args[0] -eq 'remove') {
+        # Remove edge policies
+        Add-Content -Path $Path_ElevateCMD -Value "reg import `"$Path_RemoveREG`""
+        Start-Process $Path_ElevateCMD -Wait
+    
+    } elseif ($args[0] -match '^[a-p]{32}$') {
+        # Custom new tab addon to replace Tabliss
+        $ReplaceString = @(
+            "set `"_source=$Path_AddREG`"",
+            "set `"_replaced=$Path_ReplaceREG`"",
+            '',
+            '> "%_replaced%" (',
+            '    for /f "delims=" %%A in (%_source%) do (',
+            '        set "line=%%A"',
+            "        set `"line=!line:lklaendlmlfkaabeleddanafeinnenih=$args[0]!`"",
+            '        echo(!line!',
+            '    )',
+            ')',
+            '',
+            "reg import `"$Path_ReplaceREG`""
+        )
+        Add-Content -Path $Path_ElevateCMD -Value $ReplaceString
+        Start-Process $Path_ElevateCMD -Wait
+    }
+    
+    Remove-Item -Path "$env:TEMP\TmFlZW1Cb2xjaGhp" -Recurse -Force
+} else {
+    Write-Warning "Verification failed. One or more files are missing."
 }
